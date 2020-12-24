727058c73b512b6b96aef25578860f5c26dccac9
JetechNY <sunjetliu@gmail.com>
2020-12-24 02:40:26 -0500

    display historyv2

--- src/Components/History.js
+++ src/Components/History.js
@@ -5,30 +5,38 @@ import formatCurrency from "./util";
 
 export default class History extends Component {
 
+    renderItems = () => {
+        
+    }
+
+
     renderOldCart = () => {
-        console.log("history",this.props.pastOrders)
+        // console.log("history",this.props.pastOrders)
         return (
             this.props.pastOrders.map((carts)=> {
-                console.log("cartsitems", carts.menu_items)
+                // console.log("cartsitems", carts.menu_items)
                 return(
-                    carts.menu_items.map((item,i) => {
-                        <div className="mcard" key={i}>
-                        <ul className='menu-items'>
-                            <a>
-                                <img alt={item.name} src={item.img} />
-                                <p> {item.name}</p>
-                                <p> {item.description}</p>
-                            </a>
-                            <div className="product-price">
-                                <div> Price: {formatCurrency(item.price)}
+                    // Cart #: {carts.id}
+                        carts.menu_items.map((item,i) => {
+                            console.log("item",item)
+                            return(
+                                <div className="mcard" key={i}>
+                                <ul className='menu-items'>
+                                    <a>
+                                        <img alt={item.name} src={item.img} />
+                                        <p> {item.name}</p>
+                                        <p> {item.description}</p>
+                                    </a>
+                                    <div className="product-price">
+                                        <div> Price: {formatCurrency(item.price)}
+                                        </div>
+                                    </div>
+                                </ul>
                                 </div>
-                            </div>
-                        </ul>
-                        </div>
-                    }
-
+                            )
+                        })
                 )
-            )})
+            })
         )
     }
 
@@ -36,7 +44,7 @@ export default class History extends Component {
         // console.log("old cart", this.props.pastOrders)
         return (
             <div>
-                <p>Hello From History</p>
+                <p>Your Order History</p>
                 {this.renderOldCart()}
             </div>
         )
